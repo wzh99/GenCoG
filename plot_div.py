61dfe5a7f9d13bb7494a9efbf40f4bad4bc9f05e
wzh99 <wzh1999_frog@126.com>
2022-06-29 17:09:38 +0800

    Script for plotting diversity.

---
+++ plot_div.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+names = ['Muffin-Chain', 'Muffin-Cell', 'DeepTyper']
+paths = [
+    'out/muffin-dag.txt',
+    'out/muffin-template.txt',
+    'out/typefuzz-penalty4.txt'
+]
+colors = ['deepskyblue', 'limegreen', 'darkorange']
+assert len(names) == len(paths), len(names) == len(colors)
+
+data = [np.loadtxt(p) for p in paths]
+
+plt.rc('font', family='Latin Modern Sans', size=14)
+
+plt.figure(figsize=(6, 4))
+plt.gca().set_box_aspect(3 / 5)
+for i in range(len(names)):
+    plt.plot(data[i][:, 0], data[i][:, 1], color=colors[i], linewidth=2, label=names[i])
+plt.ylim(0, 0.25)
+plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
+plt.xlabel('#Vertices')
+plt.ylabel('Vertex Diversity')
+plt.legend()
+plt.savefig('out/vert-div.pdf')
+plt.show()
+
+plt.figure(figsize=(6, 4))
+plt.gca().set_box_aspect(3 / 5)
+for i in range(len(names)):
+    plt.plot(data[i][:, 0], data[i][:, 2], color=colors[i], linewidth=2, label=names[i])
+plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
+plt.ylim(0, 0.85)
+plt.xlabel('#Vertices')
+plt.ylabel('Edge Diversity')
+plt.legend()
+plt.savefig('out/edge-div.pdf')
+plt.show()
