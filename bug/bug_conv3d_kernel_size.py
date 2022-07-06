38ecdb38f5779972afe7304a1a78e268e99dcd62
wzh99 <wzh1999_frog@126.com>
2022-07-06 12:37:01 +0800

    Construct graph through Relay API.

--- bug/bug_conv3d_kernel_size.py
+++ bug/bug_conv3d_kernel_size.py
@@ -1,6 +1,6 @@
 # Fixed: https://github.com/apache/tvm/pull/11681
 
-from tvm import parser
+from tvm import relay, IRModule
 
 src = """
 #[version = "0.0.5"]
@@ -9,5 +9,9 @@ def @main(%x: Tensor[(1, 16, 32, 32, 32), float32], %w: Tensor[(4, 4, 1, 1, 1), 
 }
 """
 
-mod = parser.parse(src)
+x = relay.var('x', shape=(1, 16, 32, 32, 32))
+w = relay.var('w', shape=(4, 4, 1, 1, 1))
+y = relay.nn.conv3d(x, w, kernel_size=(1, 1, 1), channels=4, groups=4)
+mod = IRModule.from_expr(y)
+mod = relay.transform.InferType()(mod)
 print(mod)
