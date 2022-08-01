aac35e875e45b4a9113118514b6f402e667c95ed
wzh99 <wzh1999_frog@126.com>
2022-08-01 22:06:52 +0800

    Evaluate validity of Muffin in both Keras and Relay.

--- muffin_valid.py
+++ muffin_valid.py
@@ -1,9 +1,12 @@
 from argparse import ArgumentParser, Namespace
 from sys import stdout
 
+import numpy as np
 from tqdm import tqdm
+from tvm import relay, TVMError
 
 from muffin.model_generator import ModelGenerator
+from tvm_util.frontend import from_keras
 
 args = Namespace()
 
@@ -23,16 +26,35 @@ def main():
 
     # Generation loop
     progress = tqdm(range(args.number), file=stdout)
-    num_invalid = 0
-    progress.set_postfix_str(str(num_invalid))
+    keras_invalid, relay_invalid = 0, 0
+
+    def display_count():
+        progress.set_postfix_str(f'{keras_invalid}, {relay_invalid}')
+
+    progress.set_postfix_str(str(keras_invalid))
     for _ in progress:
         # Generate Keras model
         try:
-            model_gen.generate(args.mode)
+            model = model_gen.generate(args.mode)
         except ValueError as err:
             print(err)
-            num_invalid += 1
-            progress.set_postfix_str(str(num_invalid))
+            keras_invalid += 1
+            display_count()
+            continue
+
+        # Convert to Relay
+        batch_size = np.random.randint(1, 5)
+        input_shapes = {inp.name: (batch_size,) + tuple(inp.shape.as_list()[1:])
+                        for inp in model.inputs}
+        mod, params = from_keras(model, shape=input_shapes)
+
+        # Check type correctness
+        try:
+            relay.transform.InferType()(mod)
+        except TVMError:
+            relay_invalid += 1
+            display_count()
+            continue
 
 
 if __name__ == '__main__':
