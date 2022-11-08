878e0db59ead17bbadfc858b6b7cdd8441e93f9f
wzh99 <wzh1999_frog@126.com>
2022-11-08 16:35:54 +0800

    Fix coverage plot.

--- plot_cov.py
+++ plot_cov.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 names = ['LEMON', 'Muffin-Chain', 'Muffin-Cell', 'Luo-WS', 'Luo-RN', 'GenCoG-M', 'GenCoG']
 paths = [
     'out/cov-lemon/data.txt',
-    'out/cov-graphfuzz-ws/data.txt',
-    'out/cov-graphfuzz-rn/data.txt',
     'out/cov-muffin-dag/data.txt',
     'out/cov-muffin-template/data.txt',
+    'out/cov-graphfuzz-ws/data.txt',
+    'out/cov-graphfuzz-rn/data.txt',
     'out/cov-gencog-muffin/data.txt',
     'out/cov-gencog-complete/data.txt',
 ]
@@ -21,7 +21,7 @@ plt.rc('font', family='Latin Modern Sans', size=9)
 plt.figure(figsize=(5, 3.5))
 plt.gca().set_box_aspect(3 / 5)
 for i in range(len(names)):
-    plt.plot(data[i][:, 0], data[i][:, 1], color=colors[i], linewidth=2, label=names[i])
+    plt.plot(data[i][:, 0], data[i][:, 1], color=colors[i], linewidth=1.5, label=names[i])
 plt.ylim(3.05e4, 3.5e4)
 plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
 plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
